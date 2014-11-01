99bd377760eb5aa92e996d91f140d4ac22b180c6
Will ST <willst.willst@gmail.com>
2014-11-01 06:23:58 +0000

    Add new class LexPermFinder

---
+++ LexPermFinder.py
@@ -0,0 +1,2 @@
+class LexPerm(object):
+
---
+++ tests/TestLexPermFinder.py
@@ -0,0 +1,10 @@
+import unittest
+from nose.tools import *
+from mock import Mock
+
+import LexPermFinder
+
+class TextLexPerm(unittest.test):
+
+    def testOne:
+        pass
